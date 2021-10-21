bddf51e4735c3f291ecaa87f22101112b40a3a27
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-10-21 09:33:07 +0200

    style on card header

--- src/app/services/place-repo.service.ts
+++ src/app/services/place-repo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { IRecordedPlace, IPlaceToRecord } from '../models/place';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 interface IGetAllPlacesAnswer {
   message: string;
