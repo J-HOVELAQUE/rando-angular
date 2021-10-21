4b0f5f0af969c35a85d67ba03ebb1c878eaafc5a
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-10-21 10:09:59 +0200

    use old data for update and send request

--- src/app/place-list/components/edit-place-modal/edit-place-modal.component.ts
+++ src/app/place-list/components/edit-place-modal/edit-place-modal.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
 
 import { Scavenger } from '@wishtack/rx-scavenger';
+import { PlaceRepoService } from 'src/app/services/place-repo.service';
+import { IRecordedPlace } from 'src/app/models/place';
 
 @Component({
   selector: 'app-edit-place-modal',
@@ -11,20 +13,17 @@ import { Scavenger } from '@wishtack/rx-scavenger';
 })
 export class EditPlaceModalComponent implements OnInit {
   @Input() event: Observable<void>;
-  @Input() placeId: string;
-  @Input() placeName: string;
+  @Input() place: IRecordedPlace;
 
   showModal = false;
+  editPlaceForm: FormGroup;
   private _eventSubscription: Subscription;
   private _scavenger = new Scavenger(this);
 
-  constructor(private _formBuilder: FormBuilder) {}
-
-  editPlaceForm = this._formBuilder.group({
-    name: [''],
-    altitudeInMeters: [''],
-    mountainLocation: [''],
-  });
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _placeRepo: PlaceRepoService
+  ) {}
 
   onCloseModal() {
     this.showModal = false;
@@ -32,9 +31,28 @@ export class EditPlaceModalComponent implements OnInit {
 
   onEditPlace() {
     console.log(this.editPlaceForm.value);
+    const request = this._placeRepo.editPlace(
+      this.editPlaceForm.value,
+      this.place._id
+    );
+
+    request.pipe(this._scavenger.collectByKey('edit-place')).subscribe(
+      (answer) => {
+        console.log('UPDATE', answer);
+      },
+      (error) => {
+        console.log('Update fail', error);
+      }
+    );
   }
 
   ngOnInit(): void {
+    this.editPlaceForm = this._formBuilder.group({
+      name: [this.place.name],
+      altitudeInMeters: [this.place.altitudeInMeters],
+      mountainLocation: [this.place.mountainLocation],
+    });
+
     this._eventSubscription = this.event.subscribe(() => {
       this.showModal = true;
     });
