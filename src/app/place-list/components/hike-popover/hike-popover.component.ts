091072da87bbf7b7f6bcd52fe7adbcb31cc0b8f7
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-10-21 18:55:01 +0200

    display list of hike in popover

--- src/app/place-list/components/hike-popover/hike-popover.component.ts
+++ src/app/place-list/components/hike-popover/hike-popover.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 import { HikeRepoService } from 'src/app/services/hike-repo.service';
+import { IRecordedHike } from 'src/app/models/hike';
 
 import { Scavenger } from '@wishtack/rx-scavenger';
 
@@ -14,6 +15,7 @@ export class HikePopoverComponent implements OnInit, OnDestroy {
   @Input() placeId: string;
   name = 'World';
   faEye = faEye;
+  hikes: IRecordedHike[];
   private _scavenger = new Scavenger(this);
 
   constructor(private _hikeRepo: HikeRepoService) {}
@@ -22,6 +24,7 @@ export class HikePopoverComponent implements OnInit, OnDestroy {
     const request = this._hikeRepo.getHikeByPlace(this.placeId);
     request.pipe(this._scavenger.collectByKey('load-hike')).subscribe(
       (response) => {
+        this.hikes = response.hikes;
         console.log('LOADED', response);
       },
       (error) => {
