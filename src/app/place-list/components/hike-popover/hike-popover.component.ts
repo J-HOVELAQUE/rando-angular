1327172caf15be0a7ef33125ac2284bf10d633d8
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-10-21 18:48:57 +0200

    request for get hike by place

--- src/app/place-list/components/hike-popover/hike-popover.component.ts
+++ src/app/place-list/components/hike-popover/hike-popover.component.ts
@@ -1,23 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
+
+import { HikeRepoService } from 'src/app/services/hike-repo.service';
+
+import { Scavenger } from '@wishtack/rx-scavenger';
 
 @Component({
   selector: 'app-hike-popover',
   templateUrl: './hike-popover.component.html',
   styleUrls: ['./hike-popover.component.css'],
 })
-export class HikePopoverComponent implements OnInit {
+export class HikePopoverComponent implements OnInit, OnDestroy {
   @Input() placeId: string;
   name = 'World';
   faEye = faEye;
+  private _scavenger = new Scavenger(this);
 
-  constructor() {}
+  constructor(private _hikeRepo: HikeRepoService) {}
 
   onLoadHike() {
-    console.log('LOAD', this.placeId);
+    const request = this._hikeRepo.getHikeByPlace(this.placeId);
+    request.pipe(this._scavenger.collectByKey('load-hike')).subscribe(
+      (response) => {
+        console.log('LOADED', response);
+      },
+      (error) => {
+        console.error('LOADING FAILED', error);
+      }
+    );
   }
 
-  ngOnInit(): void {
-    console.log('INIT');
-  }
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {}
 }
