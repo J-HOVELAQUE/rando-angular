a05f678f080127cb86cdd2bde5309a888ed21e31
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-10-22 18:20:37 +0200

    using store on hike sheet

--- src/app/hiking-sheet/hiking-sheet.component.ts
+++ src/app/hiking-sheet/hiking-sheet.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StoreService } from '../services/store.service';
+
 @Component({
   selector: 'app-hiking-sheet',
   templateUrl: './hiking-sheet.component.html',
-  styleUrls: ['./hiking-sheet.component.css']
+  styleUrls: ['./hiking-sheet.component.css'],
 })
 export class HikingSheetComponent implements OnInit {
+  picture: string;
 
-  constructor() { }
+  constructor(public store: StoreService) {}
 
   ngOnInit(): void {
+    if (this.store.activeHike?.place?.picture) {
+      this.picture = this.store.activeHike.place.picture;
+    } else {
+      this.picture = '';
+    }
   }
-
 }
