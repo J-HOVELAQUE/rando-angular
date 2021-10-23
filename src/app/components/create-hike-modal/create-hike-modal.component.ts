5e2b0e0d39cf9f50c445b405794b320615109e9b
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-10-23 19:00:58 +0200

    preparing data for recording new hike

--- src/app/components/create-hike-modal/create-hike-modal.component.ts
+++ src/app/components/create-hike-modal/create-hike-modal.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 
 import { IRecordedPlace } from 'src/app/models/place';
+import { IRecordedParticipant } from 'src/app/models/participant';
+
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create-hike-modal',
@@ -13,12 +16,39 @@ export class CreateHikeModalComponent implements OnInit, OnDestroy {
   @Input() placeWhereCreateNewHike: IRecordedPlace;
 
   isOpen = false;
+  selectedParticipants: IRecordedParticipant[] = [];
   private _openModalSubscription: Subscription;
 
-  constructor() {}
+  constructor(private _formBuilder: FormBuilder) {}
+
+  newHikeForm = this._formBuilder.group({
+    date: [''],
+    durationInMinutes: [''],
+    elevationInMeters: [''],
+    distanceInMeters: [''],
+    startingAltitude: [''],
+    arrivalAltitude: [''],
+    description: [''],
+  });
 
   onCloseModal() {
     this.isOpen = false;
+  }
+
+  onChangeSelectedParticipants(participants: IRecordedParticipant[]) {
+    this.selectedParticipants = participants;
+  }
+
+  onSubmitNewHike() {
+    const participants: string[] = this.selectedParticipants.map(
+      (part) => part._id
+    );
+    const newHike = {
+      ...this.newHikeForm.value,
+      place: this.placeWhereCreateNewHike._id,
+      participants: participants,
+    };
+    console.log('CREATE HIKE', newHike);
   }
 
   ngOnInit(): void {
