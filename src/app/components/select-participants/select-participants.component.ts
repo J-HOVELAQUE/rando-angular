c662f5fa59b4595497041310399e4818ad91b6d5
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-10-23 18:34:54 +0200

    select participants emit event on change

--- src/app/components/select-participants/select-participants.component.ts
+++ src/app/components/select-participants/select-participants.component.ts
@@ -1,30 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  OnDestroy,
+  EventEmitter,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 import { ParticipantRepoService } from 'src/app/services/participant-repo.service';
 import { IRecordedParticipant } from 'src/app/models/participant';
 
-import { MatFormFieldAppearance } from '@angular/material/form-field';
+import { Scavenger } from '@wishtack/rx-scavenger';
 
 @Component({
   selector: 'app-select-participants',
   templateUrl: './select-participants.component.html',
   styleUrls: ['./select-participants.component.css'],
 })
-export class SelectParticipantsComponent implements OnInit {
+export class SelectParticipantsComponent implements OnInit, OnDestroy {
+  @Output() changeSelectedParticipants = new EventEmitter();
+
   participants = new FormControl();
   participantList: IRecordedParticipant[] = [];
 
+  private _scavenger = new Scavenger(this);
+
   constructor(private _participantRepo: ParticipantRepoService) {}
 
-  onGetParticipants() {
-    console.log('CLICK');
+  onChangeSelectedParticipants(value: IRecordedParticipant[]) {
+    this.changeSelectedParticipants.emit(value);
   }
 
   ngOnInit(): void {
     const request = this._participantRepo.getAllParticipants();
 
-    request.subscribe(
+    request.pipe(this._scavenger.collectByKey('get-participants')).subscribe(
       (response) => {
         this.participantList = response.places;
       },
@@ -33,4 +43,6 @@ export class SelectParticipantsComponent implements OnInit {
       }
     );
   }
+
+  ngOnDestroy(): void {}
 }
