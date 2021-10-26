7a0c5241475d5514770363c5761edd9691034963
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-10-26 15:51:18 +0200

    change coordinates in form when change place

--- src/app/place-list/components/set-location-modal/set-location-modal.component.ts
+++ src/app/place-list/components/set-location-modal/set-location-modal.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 import { Observable, Subscription } from 'rxjs';
+import { FormBuilder } from '@angular/forms';
 import { IRecordedPlace } from '../../../models/place';
 
 @Component({
@@ -8,14 +16,19 @@ import { IRecordedPlace } from '../../../models/place';
   templateUrl: './set-location-modal.component.html',
   styleUrls: ['./set-location-modal.component.css'],
 })
-export class SetLocationModalComponent implements OnInit, OnDestroy {
+export class SetLocationModalComponent implements OnInit, OnDestroy, OnChanges {
   @Input() openModalSwitcher: Observable<void>;
   @Input() place: IRecordedPlace;
 
   isOpen = false;
+  coordForm = this._formBuilder.group({
+    lat: [''],
+    long: [''],
+  });
+
   private _openModalListener: Subscription;
 
-  constructor() {}
+  constructor(private _formBuilder: FormBuilder) {}
 
   onClose() {
     this.isOpen = false;
@@ -28,6 +41,20 @@ export class SetLocationModalComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this._openModalListener = this.openModalSwitcher.subscribe(() => {
       this.isOpen = true;
+    });
+  }
+
+  ngOnChanges(change: SimpleChanges): void {
+    if (change.place && this.place.location.coordinates.length === 2) {
+      this.coordForm.setValue({
+        lat: this.place.location.coordinates[0],
+        long: this.place.location.coordinates[1],
+      });
+      return;
+    }
+    this.coordForm.setValue({
+      lat: '0',
+      long: '0',
     });
   }
 
