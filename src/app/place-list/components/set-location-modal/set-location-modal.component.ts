960a1aa27063625dbec46966f6bbd059394afc8a
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-10-26 18:30:54 +0200

    change location

--- src/app/place-list/components/set-location-modal/set-location-modal.component.ts
+++ src/app/place-list/components/set-location-modal/set-location-modal.component.ts
@@ -9,6 +9,8 @@ import {
 
 import { Observable, Subscription, Subject } from 'rxjs';
 import { IRecordedPlace } from '../../../models/place';
+import { PlaceRepoService } from '../../../services/place-repo.service';
+import { Scavenger } from '@wishtack/rx-scavenger';
 
 interface ICoords {
   lat: string;
@@ -33,11 +35,24 @@ export class SetLocationModalComponent implements OnInit, OnDestroy, OnChanges {
   };
 
   private _openModalListener: Subscription;
+  private _scavenger = new Scavenger(this);
 
-  constructor() {}
+  constructor(private _placeRepo: PlaceRepoService) {}
 
   onClose() {
     this.isOpen = false;
+  }
+
+  onSubmitNewCoordinates() {
+    const request = this._placeRepo.setPlaceLocation(
+      this.coords,
+      this.place._id
+    );
+
+    request.pipe(this._scavenger.collectByKey('set-location')).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    );
   }
 
   onChangeCoordinates(coords: ICoords) {
