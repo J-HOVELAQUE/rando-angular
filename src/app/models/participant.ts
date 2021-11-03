ddcb1f3b00e7aca7a663327eaf13ab548488d919
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-11-03 10:22:09 +0100

    implementation of participant data in store

--- src/app/models/participant.ts
+++ src/app/models/participant.ts
@@ -8,3 +8,21 @@ export interface IParticipantToCreate {
 export interface IRecordedParticipant extends IParticipantToCreate {
   _id: string;
 }
+
+interface IParticipantDataForAMonth {
+  _id: {
+    year: number;
+    month: number;
+  };
+  nb_hike: number;
+  total_dist: number;
+  total_elev: number;
+  total_time: number;
+}
+
+export interface IParticipantData {
+  participantId: string;
+  participantName: string;
+  participantFirstname: string;
+  data: IParticipantDataForAMonth[];
+}
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { IRecordedHike } from '../models/hike';
+import { IParticipantData } from '../models/participant';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StoreService {
   activeHike: IRecordedHike;
+  participantData: IParticipantData;
 
   constructor() {}
 }
