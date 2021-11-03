0c953c0e8f684dc07784c77c6e2cb3de6d113ce5
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-11-03 18:58:15 +0100

    display hike for a participant

--- src/app/participants/components/hiking-list/hiking-list.component.ts
+++ src/app/participants/components/hiking-list/hiking-list.component.ts
@@ -1,12 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HikeRepoService } from 'src/app/services/hike-repo.service';
+import { StoreService } from 'src/app/services/store.service';
+import { Subscription } from 'rxjs';
+
+import { IRecordedHike } from 'src/app/models/hike';
+
+import { Scavenger } from '@wishtack/rx-scavenger';
 
 @Component({
   selector: 'app-hiking-list',
   templateUrl: './hiking-list.component.html',
   styleUrls: ['./hiking-list.component.css'],
 })
-export class HikingListComponent implements OnInit {
-  constructor() {}
+export class HikingListComponent implements OnInit, OnDestroy {
+  hikes: IRecordedHike[];
+  private _scavenger = new Scavenger(this);
+  private _changeParticipantListener: Subscription;
 
-  ngOnInit(): void {}
+  constructor(public store: StoreService, private _hikeRepo: HikeRepoService) {}
+
+  onLoadHikeList() {
+    if (this.store.participantData.participantId) {
+      this._hikeRepo
+        .getHikeByUser(this.store.participantData.participantId)
+        .pipe(this._scavenger.collectByKey('get-hike-by-user'))
+        .subscribe(
+          (response) => {
+            this.hikes = response.hikes;
+          },
+          (error) => console.log(error)
+        );
+    }
+  }
+
+  ngOnInit(): void {
+    this._changeParticipantListener = this.store.participantAsChanged.subscribe(
+      () => {
+        this.onLoadHikeList();
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._changeParticipantListener.unsubscribe();
+  }
 }
