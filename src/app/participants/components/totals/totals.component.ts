7e457f2447d965125e2784ef1fe2efef349233a4
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-11-04 11:35:46 +0100

    display participant totals

--- src/app/participants/components/totals/totals.component.ts
+++ src/app/participants/components/totals/totals.component.ts
@@ -1,13 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { StoreService } from 'src/app/services/store.service';
+import { Subscription } from 'rxjs';
+
+interface ITotals {
+  totalHikes: number;
+  totalElevation: number;
+  totalDistance: number;
+  totalDuration: number;
+}
 
 @Component({
   selector: 'app-totals',
   templateUrl: './totals.component.html',
   styleUrls: ['./totals.component.css'],
 })
-export class TotalsComponent implements OnInit {
+export class TotalsComponent implements OnInit, OnDestroy {
+  totals: ITotals;
+  private _changeParticipantListener: Subscription;
+
   constructor(public store: StoreService) {}
 
-  ngOnInit(): void {}
+  onCalculateTotals() {
+    this.totals = this.store.participantData.data.reduce(
+      (totals: ITotals, currentMonth) => {
+        totals.totalHikes += currentMonth.nb_hike;
+        totals.totalElevation += currentMonth.total_elev;
+        totals.totalDistance += currentMonth.total_dist;
+        totals.totalDuration += currentMonth.total_time;
+        return totals;
+      },
+      { totalHikes: 0, totalElevation: 0, totalDistance: 0, totalDuration: 0 }
+    );
+  }
+
+  ngOnInit(): void {
+    this._changeParticipantListener = this.store.participantAsChanged.subscribe(
+      () => {
+        this.onCalculateTotals();
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._changeParticipantListener.unsubscribe();
+  }
 }
